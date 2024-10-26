b6f63e58277ff972d01614341645ce6ee3e24e1c
Rafael Rier <rafael.riber@epfl.ch>
2024-10-26 02:23:46 +0200

    Figured out clocks ! (hopefully)

--- src/audio_fpga/i2s.py
+++ src/audio_fpga/i2s.py
@@ -7,9 +7,13 @@ class I2S_clocks(wiring.Component):
     en : In(1)
     mclk : Out(1)
     sclk : Out(1)
-    ws : Out(1)
-    def __init__(self, f_sample=44100, mclk_sclk_ratio=4, sclk_ws_ratio=64):
-        self.f_sample = f_sample
+    ws : Out(1) 
+
+    # ws is the sample rate of the interface, set mclk_freq = f_s * mclk_sclk_ratio * sclk_ws_ratio
+    # For example : f_sample = 44100 Hz --> mclk_freq = 44100 * 64 * 4 = 11.29 MHz
+
+
+    def __init__(self, mclk_sclk_ratio=4, sclk_ws_ratio=64):
         self.mclk_sclk_ratio = mclk_sclk_ratio
         self.sclk_ws_ratio = sclk_ws_ratio
         super().__init__()
@@ -19,29 +23,26 @@ class I2S_clocks(wiring.Component):
 
         m.domains.mclk = cd_mclk = ClockDomain(local=True)
 
-        sclk_count = Signal(range(self.mclk_sclk_ratio+1))
+        sclk_count = Signal(range(self.mclk_sclk_ratio + 1))
         sclk_ovf = Signal()
 
-        ws_count = Signal(range((2*self.mclk_sclk_ratio * self.sclk_ws_ratio) + self.mclk_sclk_ratio))
-
-        ws_ovf = Signal()
+        ws_count = Signal(range(2*self.sclk_ws_ratio*self.mclk_sclk_ratio))
+        ws_ovf = Signal(init=0)
         
-        # TODO: Fix the overflows so that everything is aligned
         m.d.comb += self.mclk.eq(cd_mclk.clk)
-        m.d.comb += sclk_ovf.eq(sclk_count == 2**(sclk_count.shape().width-1)-1) 
-        m.d.comb += ws_ovf.eq(ws_count == (2*self.mclk_sclk_ratio * self.sclk_ws_ratio) + self.mclk_sclk_ratio)
-
-
+        m.d.comb += sclk_ovf.eq(sclk_count == 2**(sclk_count.shape().width-1)-1)
+        m.d.comb += ws_ovf.eq(ws_count == (2**(ws_count.shape().width)-1)) 
         with m.If(self.en):
             m.d.mclk += sclk_count.eq(sclk_count + 1)
             m.d.mclk += ws_count.eq(ws_count + 1)
             with m.If(sclk_ovf):
                 m.d.mclk += sclk_count.eq(0)
                 m.d.mclk += self.sclk.eq(~self.sclk)
+
             with m.If(ws_ovf):
                 m.d.mclk += ws_count.eq(0)
                 m.d.mclk += self.ws.eq(~self.ws)
-
+                
 
 
 
--- tests/test_i2s.py
+++ tests/test_i2s.py
@@ -4,8 +4,8 @@ from audio_fpga import I2S_clocks
 
 
 def test_i2s_clocks():
-    m_s_ratio = 4 
-    s_w_ratio = 264
+    m_s_ratio = 4
+    s_w_ratio = 64 
 
     i2s_clocks = I2S_clocks(mclk_sclk_ratio=m_s_ratio, sclk_ws_ratio=s_w_ratio)
 
@@ -13,14 +13,20 @@ def test_i2s_clocks():
         # Wait a few cycles
         await ctx.tick(domain='mclk').repeat(3)
         ctx.set(i2s_clocks.en, True)
-        for _ in range(100):
-            # await ctx.tick(domain='mclk').repeat(m_s_ratio-1)
-            # assert ctx.get(i2s_clocks.sclk) == 0
+        # Test mclk/sclk ratio
+        for _ in range(m_s_ratio - 1):
             await ctx.tick(domain='mclk').repeat(m_s_ratio)
             assert ctx.get(i2s_clocks.sclk) == 1
             await ctx.tick(domain='mclk').repeat(m_s_ratio)
             assert ctx.get(i2s_clocks.sclk) == 0
+        
+        # Test sclk/ws ratio
 
+        for _ in range(s_w_ratio-1):
+            await ctx.tick(domain='mclk').repeat(2*m_s_ratio*s_w_ratio+1)
+            assert ctx.get(i2s_clocks.ws) == 1
+            await ctx.tick(domain='mclk').repeat(2*m_s_ratio*s_w_ratio)
+        await ctx.tick(domain='mclk').repeat(m_s_ratio*10)
 
 
     sim = Simulator(i2s_clocks)
